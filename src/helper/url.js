71d2054069873d689494470d2c48e4fd9a35875e
bobross96 <zadus96@gmail.com>
2021-07-05 19:55:25 +0800

    added the process env to hide url

--- src/helper/url.js
+++ src/helper/url.js
@@ -1,6 +1,6 @@
 // let API_URL = "https://ntu-toilet-app.herokuapp.com"
 
 //if in production, let API_URL = "http://localhost:4000"
-let API_URL  = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://ntu-toilet-app.herokuapp.com' 
+let API_URL  = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.API_URL
 
 export default API_URL
