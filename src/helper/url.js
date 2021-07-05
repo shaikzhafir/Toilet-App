8245fa74b49e1a4bee8e299a7ca41536b6817f19
bobross96 <zadus96@gmail.com>
2021-07-05 19:59:43 +0800

    added react app to the env variable

--- src/helper/url.js
+++ src/helper/url.js
@@ -1,6 +1,6 @@
 // let API_URL = "https://ntu-toilet-app.herokuapp.com"
 
 //if in production, let API_URL = "http://localhost:4000"
-let API_URL  = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.API_URL
+let API_URL  = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.REACT_APP_API_URL
 
 export default API_URL
